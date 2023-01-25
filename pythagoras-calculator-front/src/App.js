1c3d8f6ac517189590d7ecb13e597b5ca719da44
Gian Bueno <gianbuenor@gmail.com>
2023-01-25 09:36:28 -0300

    te

---
+++ pythagoras-calculator-front/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Form from '../src/components/Form/index.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Form></Form>
+    </div>
+  );
+}
+
+export default App;
